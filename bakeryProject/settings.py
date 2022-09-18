bdc68377f416fb89811456e58c9d28a6187eb9c1
painstyle01 <painstyle01@gmail.com>
2022-09-18 08:15:18 +0000

    Save

--- bakeryProject/settings.py
+++ bakeryProject/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "api"
+    "api",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
@@ -50,9 +51,12 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware"
 ]
 
 ROOT_URLCONF = "bakeryProject.urls"
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
@@ -126,3 +130,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- bakeryProject/urls.py
+++ bakeryProject/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import routers
 from api import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 router = routers.DefaultRouter()
 
@@ -29,3 +32,4 @@ urlpatterns = [
     re_path(r"^api/", include(router.urls)),
     path('api-auth/', include('rest_framework.urls'))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
